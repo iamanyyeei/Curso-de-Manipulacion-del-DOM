f829f34afd4ab3acf224aeff4fcac42acbd5e68e
iamanyyeei <iamanyyeei@gmail.com>
2025-05-10 14:39:36 -0400

    Modificar estilos con .className y con .style

---
+++ Manipulación de Elementos/3. Modificar estilos CSS/app.js
@@ -0,0 +1,23 @@
+/* MODIFICAR ESTILOS */
+
+// ----- MÉTODO NRO 1 .STYLE ------
+
+//Cambiar el color de una fuente
+const title = document.querySelector("h1");
+console.log(title);
+
+title.style.color ="pink";
+
+//Cambiar el Background
+const menu = document.querySelector(".menu");
+console.log(menu);
+
+menu.style.backgroundColor = "pink";
+
+//Cambiar el size de un texto
+menu.style.fontSize = "2rem";
+
+// ----- MÉTODO NRO 2 .CLASSNAME ------
+
+//Sustituye la clase .menu por .main-menu que coloca el texto en color verde
+menu.className = "main-menu";
