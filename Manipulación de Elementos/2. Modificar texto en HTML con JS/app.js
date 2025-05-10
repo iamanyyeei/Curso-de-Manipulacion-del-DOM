a25ca25b94ccc7481a78cd625fdbf9f10a183a78
iamanyyeei <iamanyyeei@gmail.com>
2025-05-10 13:12:02 -0400

    Métodos para modificar un texto HTML desde JS

---
+++ Manipulación de Elementos/2. Modificar texto en HTML con JS/app.js
@@ -0,0 +1,11 @@
+const title = document.querySelector("#app-title");
+console.dir(title);
+
+//Método Nro. 1
+title.textContent = "Soy un nuevo texto";
+
+//Método Nro. 2
+const listItem = document.querySelector("li");
+console.dir(listItem);
+
+listItem.innerHTML = "Epale, cambié de nombre";
